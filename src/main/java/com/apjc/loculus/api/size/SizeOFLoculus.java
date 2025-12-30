00a31066ca8e4e511ad9b7c4516ec582739de0cc
AP-JavaCode <megafoh3g@gmail.com>
2025-12-30 12:00:38 +0500

    difference update

--- src/main/java/com/apjc/loculus/api/size/SizeOFLoculus.java
+++ src/main/java/com/apjc/loculus/api/size/SizeOFLoculus.java
@@ -9,30 +9,54 @@ import com.apjc.loculus.FileLoculus;
 public class SizeOFLoculus {
 	
 	public static <T> boolean isCompressionEffectiveObj(List<T> data) {
-		SizeOf size = SizeOf.newInstance();
-		Loculus<T> loculus = new Loculus<>(data);
-		long listSize = size.sizeOf(data);
-		long loculusSize = size.sizeOf(loculus);
-		loculus.close();
-		return listSize > loculusSize;
+		Sizer sizer = calculateObj(data);
+		return sizer.sizeList() > sizer.sizeLoculus();
+	}
+	
+	public static <T> long differenceObj(List<T> data) {
+		Sizer sizer = calculateObj(data);
+		return sizer.sizeList() - sizer.sizeLoculus();
 	}
 	
 	public static <T extends Serializable> boolean isCompressionEffectiveWrite(List<T> data) throws IOException{
-		File file = new File("SizeOf-test.txt");
-		writeList(data, file);
-		long sizeList = file.length();
-		Loculus<T> loculus = new Loculus<>(data);
-		FileLoculus.serializableFile(loculus, file);
-		loculus.close();
-		long sizeLoculus = file.length();
-		file.delete();
-		return sizeList > sizeLoculus;
+		Sizer sizer = calculateWrite(data);
+		return sizer.sizeList() > sizer.sizeLoculus();
+	}
+	
+	public static <T extends Serializable> long differenceWrite(List<T> data) throws IOException{
+		Sizer sizer = calculateWrite(data);
+		return sizer.sizeList() - sizer.sizeLoculus();
 	}
 	
 	private static <T> void writeList(List<T> list, File file) throws IOException{
 		try(ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(file))) {
 			oos.writeObject(list);
 		}
+	}
+	
+	private static <T> Sizer calculateObj(List<T> list) {
+		SizeOf size = SizeOf.newInstance();
+		Loculus<T> loculus = new Loculus<>(list);
+		Sizer sizer = new Sizer(size.deepSizeOf(list), size.deepSizeOf(loculus));
+		loculus.close();
+		return sizer;
+	}
+	
+	private static <T extends Serializable> Sizer calculateWrite(List<T> list) throws IOException{
+		File file = new File("sizeOf-test.txt");
+		writeList(list, file);
+		long sizeList = file.length();
+		file.delete();
+		Loculus<T> loculus = new Loculus<T>(list);
+		FileLoculus.serializableFile(loculus, file);
+		loculus.close();
+		long sizeLoculus = file.length();
+		file.delete();
+		return new Sizer(sizeList, sizeLoculus);
+	}
+	
+	private static  record Sizer(long sizeList, long sizeLoculus){
+		
 	}
 
 }
